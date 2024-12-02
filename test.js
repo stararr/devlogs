6ac967f25b7bec9afb5d307b5cc4f07c4193fc54
stararr <stararrr.25@gmail.com>
2024-12-02 19:50:32 +0530

    clipboard added + trailing spaces removed (again)

--- test.js
+++ test.js
@@ -1,3 +1,7 @@
+function elem(id) {
+    return document.getElementById(id)
+}
+
 for (code of document.getElementsByTagName("code")) {
     console.log("hi", code)
     code.innerHTML = code.innerHTML.trim()
@@ -6,4 +10,31 @@ for (code of document.getElementsByTagName("code")) {
 /*padding: 0 2rem;
 border: none;*/
 
-navigator.clipboard.writeText("")
+var cur
+function toggleCode(button) {
+    console.log(button.nextElementSibling.style)
+    switch (button.nextElementSibling.style.display) {
+        case 'block' || '':
+            console.log('hi')
+            button.nextElementSibling.style.maxHeight = '0';
+            button.nextElementSibling.style.padding = '0 2rem';
+            button.nextElementSibling.style.border = 'none';
+        case 'none':
+            console.log("HELLO")
+            button.nextElementSibling.style.maxHeight = '';
+            button.nextElementSibling.style.padding = '';
+            button.nextElementSibling.style.border = '';
+    }
+}
+
+var copynotif = elem("copynotif")
+for (clipboard of document.getElementsByClassName("clipboard")) {
+    clipboard.addEventListener("click", function(e) {
+        console.log('clicked')
+        navigator.clipboard.writeText(clipboard.parentElement.innerText)
+        copynotif.style.filter = "opacity(1)"
+        window.setTimeout(() => {
+            copynotif.style.filter = "opacity(0)"
+        }, 1250);
+    })
+}
